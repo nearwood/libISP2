fd62bd7f47ed63cc27e21a868e2a2dca105b1406
Nick <nearwood@gmail.com>
2019-12-17 20:13:35 -0600

    Reformat, cleanup code and remove comments

--- src/example/isp2.cpp
+++ src/example/isp2.cpp
@@ -3,24 +3,35 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+//TODO #include <iostream>
 
 int main() {
-	int fd;
-	isp2_t data;
+  int fd;
+  isp2_t data;
 
-	fd = open("data/lc-2.bin",O_RDONLY);
-	if (fd == -1) {
-		printf("Open error:(%d) %s\n",errno,strerror(errno));
-	}
-	
-	while(ISP2::isp2_read(fd,data) != -1) {
-		printf("Packet length: %d\n",data.packet_length);
-		printf("Lambda: %f\n",data.lambda/1000.0);
-		printf("Status: %d\n",data.status);
-		printf("afr_m: %f\n",data.afr_multiplier/10.0);
-		if(data.is_recording){printf("Recording\n");}
-		if(data.is_sensor_data){printf("Sensor Data\n");}
-		if(data.sender_can_log){printf("Can Log\n");}
-	}
-    return 0;
+  fd = open("data/lc-2.bin", O_RDONLY);
+  if (fd == -1) {
+    printf("Open error:(%d) %s\n", errno, strerror(errno));
+  }
+  
+  while (ISP2::isp2_read(fd,data) != -1) {
+    printf("Packet length: %d\n", data.packet_length);
+    printf("Lambda: %f\n", data.lambda / 1000.0);
+    printf("Status: %d\n", data.status);
+    printf("afr_m: %f\n", data.afr_multiplier / 10.0);
+
+    if (data.is_recording) {
+      printf("Recording\n");
+    }
+
+    if (data.is_sensor_data) {
+      printf("Sensor Data\n");
+    }
+    
+    if (data.sender_can_log) {
+      printf("Can Log\n");
+    }
+  }
+  
+  return 0;
 }
