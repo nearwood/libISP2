201841ef469a7c0c9a6f983ff996d26614dd36cf
Nick <nearwood@gmail.com>
2019-12-18 10:41:34 -0600

    Format status message and reorganize data output

--- src/example/isp2.cpp
+++ src/example/isp2.cpp
@@ -12,6 +12,29 @@
 #define ISP2_FLAGS_VERBOSE 0x01
 #define ISP2_FLAGS_DELAY 0x02
 
+const char* statusMessage(int status) {
+  switch (status) {
+    case isp2_status::ISP2_NORMAL:
+      return "NORMAL";
+    case isp2_status::ISP2_O2:
+      return "O2 1/10%";
+    case isp2_status::ISP2_CALIBRATING:
+      return "CALIBRATING";
+    case isp2_status::ISP2_NEED_CALIBRATION:
+      return "NEED CALIB";
+    case isp2_status::ISP2_WARMING:
+      return "WARMING UP";
+    case isp2_status::ISP2_HEATER_CALIBRATING:
+      return "HTR CALIB";
+    case isp2_status::ISP2_LAMBDA_ERROR_CODE:
+      return "ERROR";
+    case isp2_status::ISP2_RESERVED:
+      return "RESERVED";
+    default:
+      return "UNKNOWN"; //TODO return string value of int
+  }
+}
+
 int main(int argc, char *argv[]) {
   int fd = 0;
   int flags = 0;
@@ -52,20 +75,26 @@ int main(int argc, char *argv[]) {
   
   while (ISP2::isp2_read(fd, data) != -1) {
     //mvprintw(1, 2, "Packet length: %d\n", data.packet_length);
-    mvprintw(1, 0, "Status: %d", data.status);
-    mvprintw(2, 0, "Lambda: %0.2f", data.lambda / 1000.0);
-    mvprintw(2, 16, "AFR: %2.2f", data.afr_multiplier / 10.0);
+    mvprintw(2, 0, "Status: %-12s", statusMessage(data.status));
+    mvprintw(3, 0, "Lambda: %0.2f", data.lambda / 1000.0);
+    mvprintw(3, 13, "(Stoich: %2.2f)", data.afr_multiplier / 10.0);
 
     if (data.is_sensor_data) {
-      mvprintw(1, 12, "Sensor Data\n");
+      mvprintw(1, 0, "Data: [Sensor]");
+    } else {
+      mvprintw(1, 0, "Data: [      ]");
     }
 
     if (data.is_recording) {
-      mvprintw(1, 24, "Recording\n");
-    }    
+      mvprintw(1, 16, "RECORDING: [ON ]");
+    } else {
+      mvprintw(1, 16, "RECORDING: [OFF]");
+    }
     
     if (data.sender_can_log) {
-      mvprintw(1, 36, "Can Log\n");
+      mvprintw(1, 34, "LOG: [COULD]"); //TODO what?
+    } else {
+      mvprintw(1, 34, "LOG: [NOPE ]"); //TODO what?
     }
 
     refresh();
